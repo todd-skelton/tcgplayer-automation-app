7b2d3aea5d6537daf867a3e159418e1505300da6
Todd Skelton <todd.skelton@outlook.com>
2025-08-14 14:30:57 -0500

    Remove the timeout

--- app/httpClient.ts
+++ app/httpClient.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  timeout: 5000,
   headers: {
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
